df0e49bc1bd4d3a73ad0f0336148b8de2de5617d
llb <llb@DESKTOP-CHU2UQJ>
2018-07-14 16:57:46 +0800

    x

--- springCloud/springBoot/src/main/java/com/warmme/study/spring/boot/Controllers/HelloController.java
+++ springCloud/springBoot/src/main/java/com/warmme/study/spring/boot/Controllers/HelloController.java
@@ -1,16 +1,10 @@
 package com.warmme.study.spring.boot.Controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.warmme.study.spring.boot.config.Config;
-
 @RestController
 public class HelloController {
-	
-	@Autowired
-	private Config config;
 
 	@RequestMapping("/hello")
 	public String index() {
--- springCloud/springBoot/src/main/java/com/warmme/study/spring/boot/config/Config.java
+++ springCloud/springBoot/src/main/java/com/warmme/study/spring/boot/config/Config.java
@@ -1,19 +1,8 @@
 package com.warmme.study.spring.boot.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Config {
 
-	@Value("${test}")
-	private String test;
-
-	public String getTest() {
-		return test;
-	}
-
-	public void setTest(String test) {
-		this.test = test;
-	}
 }
