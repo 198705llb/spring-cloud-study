88b4a4afba5d293807ab89c2b68eec6b7f45b64e
llb <llb@DESKTOP-CHU2UQJ>
2018-09-07 08:22:12 +0800

    euraka

---
+++ springCloud/eureka/src/main/java/com/warmme/study/eureka/App.java
@@ -0,0 +1,13 @@
+package com.warmme.study.eureka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
