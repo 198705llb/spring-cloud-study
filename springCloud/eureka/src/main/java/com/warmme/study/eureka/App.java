d5181637a8ee08a900e529c0861df63af441b3f8
llb <llb@DESKTOP-CHU2UQJ>
2018-09-26 07:51:47 +0800

    ..

--- springCloud/eureka/src/main/java/com/warmme/study/eureka/App.java
+++ springCloud/eureka/src/main/java/com/warmme/study/eureka/App.java
@@ -1,13 +1,21 @@
 package com.warmme.study.eureka;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * Hello world!
  *
  */
+@EnableEurekaServer
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+//    	SpringApplication.run(App.class, args);
+    	new SpringApplicationBuilder(App.class).web(true).run(args);
     }
 }
