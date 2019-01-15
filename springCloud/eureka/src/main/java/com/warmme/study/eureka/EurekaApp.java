8ea90e9982d8373fd7d0b255a1c14279672fca9f
mr <904860275@qq.com>
2019-01-16 00:29:12 +0800

    test

--- springCloud/eureka/src/main/java/com/warmme/study/eureka/EurekaApp.java
+++ springCloud/eureka/src/main/java/com/warmme/study/eureka/EurekaApp.java
@@ -1,21 +1,17 @@
 package com.warmme.study.eureka;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Hello world!
- *
  */
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaApp 
-{
-    public static void main( String[] args )
-    {
+public class EurekaApp {
+    public static void main(String[] args) {
 //    	SpringApplication.run(App.class, args);
-    	new SpringApplicationBuilder(EurekaApp.class).web(true).run(args);
+        new SpringApplicationBuilder(EurekaApp.class).web(true).run(args);
     }
 }
--- springCloud/zuul/src/main/java/com/warmme/study/zuul/ZuulApp.java
+++ springCloud/zuul/src/main/java/com/warmme/study/zuul/ZuulApp.java
@@ -1,18 +1,16 @@
 package com.warmme.study.zuul;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * Hello world!
- *
  */
 @SpringCloudApplication
 @EnableZuulProxy
 public class ZuulApp {
-	public static void main(String[] args) {
-		 SpringApplication.run(ZuulApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApp.class, args);
+    }
 }
